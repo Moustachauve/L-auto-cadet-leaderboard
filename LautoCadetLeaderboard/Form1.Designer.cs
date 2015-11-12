de26e07eb42abf9893f13de67d912a305a9a63b0
Christophe Gagnier <admin@cgagnier.ca>
2015-11-12 17:08:43 -0500

    New chromium engine

--- LautoCadetLeaderboard/Form1.Designer.cs
+++ LautoCadetLeaderboard/Form1.Designer.cs
@@ -28,26 +28,13 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.webControl1 = new Awesomium.Windows.Forms.WebControl(this.components);
 			this.SuspendLayout();
-			// 
-			// webControl1
-			// 
-			this.webControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.webControl1.Location = new System.Drawing.Point(0, 0);
-			this.webControl1.Size = new System.Drawing.Size(780, 493);
-			this.webControl1.TabIndex = 0;
-			this.webControl1.JavascriptMessage += new Awesomium.Core.JavascriptMessageEventHandler(this.OnJavascriptMessage);
-			this.webControl1.TitleChanged += new Awesomium.Core.TitleChangedEventHandler(this.Awesomium_Windows_Forms_WebControl_TitleChanged);
-			this.webControl1.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Awesomium_Windows_Forms_WebControl_PreviewKeyDown);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(780, 493);
-			this.Controls.Add(this.webControl1);
 			this.DoubleBuffered = true;
 			this.MinimumSize = new System.Drawing.Size(545, 380);
 			this.Name = "Form1";
@@ -59,8 +46,6 @@
 		}
 
 		#endregion
-
-		private Awesomium.Windows.Forms.WebControl webControl1;
 	}
 }
 
